a1d91e608959600e9bb07cf9752ed511483b2474
Taylor Dawson <taylorjdawson@gmail.com>
2022-08-10 16:24:26 -0700

    initial commit

---
+++ .vitebook/config.ts
@@ -0,0 +1,17 @@
+import { clientPlugin, defineConfig } from '@vitebook/client/node';
+import { svelteMarkdownPlugin } from '@vitebook/markdown-svelte/node';
+import { defaultThemePlugin, DefaultThemeConfig } from '@vitebook/theme-default/node';
+
+export default defineConfig<DefaultThemeConfig>({
+  include: ['src/**/*.md', 'src/**/*.story.svelte'],
+  plugins: [
+    svelteMarkdownPlugin(),
+    clientPlugin({ appFile: 'App.svelte' }),
+    defaultThemePlugin(),
+  ],
+  site: {
+    title: 'Vitebook Parse Error',
+    description: 'Example showcasing the vitebook parse error',
+    theme: {},
+  },
+});
---
+++ src/index.md
@@ -0,0 +1,10 @@
+# Hello
+
+```
+const x = 0
+const templateString = `${x}`
+```
+
+```
+// `This` also won't work
+```
